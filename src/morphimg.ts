d7bab0c303e4f236045d1a133ec970eb85f21d13
José Carlos HR <joecarlhr@gmail.com>
2023-05-09 01:09:12 +0200

    implement incMapCache to improve performance when many forces are present

--- src/morphimg.ts
+++ src/morphimg.ts
@@ -15,6 +15,11 @@ interface IForce {
 	dest_y: number;
 }
 
+interface IForceInc {
+	x: number;
+	y: number;
+}
+
 export default class Morphimg {
 
 	uvmode: number = -1;
@@ -23,15 +28,13 @@ export default class Morphimg {
 
 	selected_force_id: number = -1;
 
-	selected_force: IForce;
+	editing: boolean = false;
 
-	active_rendering: boolean = false;
+	moving_node: number;
 
 	force_mult: number = 150;
 
 	focus_val: number = 300;
-
-	moving_node: number;
 
 	percentage: number = 1;
 
@@ -49,7 +52,7 @@ export default class Morphimg {
 
 	height: number;
 
-	working: boolean = false;
+	incMapCache: IForceInc[] = [];
 
 
 	constructor(params: IMorphimgParams) {
@@ -92,39 +95,69 @@ export default class Morphimg {
 			console.log('Image loaded');
 		};
 
+		let semiMapping = [];
 		overlay.addEventListener('mousemove', (evt: MouseEvent) => {
 
-			if (this.working) {
-				console.log('xhe!'); 
-				return;
-			}
-
-			if (!this.active_rendering) return;
+			if (!this.editing) return;
+			const selectedForce = this.forces[this.selected_force_id];
 			//const bbox = overlay.getBoundingClientRect();
 			const mx = evt.offsetX;// - bbox.left;
 			const my = evt.offsetY;// - bbox.top;
 			//const mx = mouse.layerX, my = mouse.layerY;
 			if (this.moving_node === 2) {
-				this.selected_force.dest_x = mx;
-				this.selected_force.dest_y = my;
+				selectedForce.dest_x = mx;
+				selectedForce.dest_y = my;
 			}
 			if (this.moving_node === 1) {
-				this.selected_force.orig_x = mx;
-				this.selected_force.orig_y = my;
+				selectedForce.orig_x = mx;
+				selectedForce.orig_y = my;
 			}
 			this.drawForces();
 
-			this.calcForcesMapping();
+			this.incMapCache = [];
 			
-			this.deform();
+			const dataLength = this.data.length / 4;
+
+			for (let i = 0; i < dataLength; i++) {
+				
+				const y = Math.floor(i / this.width);
+				const x = i - y * this.width;
+				const fIncs = this.calcForceIncs(x, y, selectedForce);
+				
+				this.incMapCache.push({
+					x: semiMapping[i].x + fIncs.x,
+					y: semiMapping[i].y + fIncs.y,
+				});
+			}
+			
+			this.render();
 
 		});
 		overlay.addEventListener('mousedown', (evt: MouseEvent) => {
+
 			const mx = evt.offsetX;
 			const my = evt.offsetY;
-			this.createForce(mx, my); 
+			this.createForce(mx, my);
+			this.calcForcesMapping();
+			semiMapping = [];
+			
+			const dataLength = this.data.length / 4;
+			const selectedForce = this.forces[this.selected_force_id];
+		
+			for (let i = 0; i < dataLength; i++) {
+				
+				const y = Math.floor(i / this.width);
+				const x = i - y * this.width;
+				const fIncs = this.calcForceIncs(x, y, selectedForce);
+				
+				semiMapping.push({
+					x: this.incMapCache[i].x - fIncs.x,
+					y: this.incMapCache[i].y - fIncs.y,
+				});
+			}
+
 		});
-		overlay.addEventListener('mouseup', () => { this.active_rendering = false; });
+		overlay.addEventListener('mouseup', () => { this.editing = false; });
 		overlay.addEventListener('mouseleave', () => {
 			this.overlay_ctx.clearRect(0, 0, canvas.width, canvas.height);
 		});
@@ -140,7 +173,7 @@ export default class Morphimg {
 				this.forces.splice(this.selected_force_id, 1);
 				this.selected_force_id = -1;
 				this.drawForces();
-				this.deform();
+				this.refresh();
 			}
 		};
 
@@ -152,7 +185,7 @@ export default class Morphimg {
 
 
 	drawImg() {
-		//if (src !== undefined) img.src = src;
+		
 		const ctx = this.ctx;
 		const img = this.img;
 		const width = this.width;
@@ -160,7 +193,6 @@ export default class Morphimg {
 		ctx.clearRect(0, 0, width, height);
 		ctx.drawImage(img, 0, 0, img.width * width / img.height, height);
 
-		
 		const imageData = ctx.getImageData(0, 0, width, height);
 		this.data = imageData.data;
 	}
@@ -179,22 +211,21 @@ export default class Morphimg {
 
 	createForce(mx: number, my: number) {
 
-		this.active_rendering = true;
+		this.editing = true;
 		//vamos a comprobar si hemos hecho click sobre una fuerza existente, en ese caso solo la seleccionaremos.
 		
 		const numForces = this.forces.length;
+		const clickRadius = 7;
 		for (let k = 0; k < numForces; k++) {
 			const force = this.forces[k];
-			if (Math.abs(mx - force.orig_x) < 7 && Math.abs(my - force.orig_y) < 7) {
+			if (Math.abs(mx - force.orig_x) < clickRadius && Math.abs(my - force.orig_y) < clickRadius) {
 				this.selected_force_id = k;
-				this.selected_force = force;
 				this.moving_node = 1;
 				this.drawForces();
 				return;
 			}
-			if (Math.abs(mx - force.dest_x) < 7 && Math.abs(my - force.dest_y) < 7) {
+			if (Math.abs(mx - force.dest_x) < clickRadius && Math.abs(my - force.dest_y) < clickRadius) {
 				this.selected_force_id = k;
-				this.selected_force = force;
 				this.moving_node = 2;
 				this.drawForces();
 				return;
@@ -209,7 +240,6 @@ export default class Morphimg {
 		});
 
 		this.selected_force_id = this.forces.length - 1;
-		this.selected_force = this.forces[this.selected_force_id];
 		this.moving_node = 2;
 		this.drawForces();
 
@@ -251,36 +281,39 @@ export default class Morphimg {
 		});
 	}
 
-	mapCache: {x: number, y: number}[];
+	calcForceIncs(x: number, y: number, force: IForce) {
+		
+		const uvmode = this.uvmode;
+		const force_mult = this.force_mult;
+		const focus_val = this.focus_val;
+
+		const force_x = force.dest_x - force.orig_x;
+		const force_y = force.dest_y - force.orig_y;	
+		const dist = Math.sqrt(Math.pow(x - force.orig_x, 2) + Math.pow(y - force.orig_y, 2));
+		//const dist = Math.sqrt(Math.pow(x - force.orig_x - force.dx, 2) + Math.pow(y - force.orig_y - force.dy, 2) );
+		const morph = uvmode * force_mult / (dist * dist / 5 + focus_val);
+		return {
+			x: force_x * morph,
+			y: force_y * morph
+		};
+
+	}
+
+	refresh() {
+		this.calcForcesMapping();
+		this.render();
+	}
 
 	calcForcesMapping() {
 		
 		const width = this.width;
-		const height = this.height;
-		const data_length = this.data.length / 4;
-		const uvmode = this.uvmode;
-		const force_mult = this.force_mult;
-		const focus_val = this.focus_val;
+		const dataLength = this.data.length / 4;
 		const forces = this.forces;
-
-		const fBaseCache = [];
 		const numForces = forces.length;
 
-		for (let k = 0; k < numForces; k++) {
-			const force = forces[k];
-			const force_x = force.dest_x - force.orig_x;
-			const force_y = force.dest_y - force.orig_y;
-			//const dist = Math.sqrt(Math.pow(x - force.orig_x - force.dx, 2) + Math.pow(y - force.orig_y - force.dy, 2) );
-			const base = uvmode * force_mult;
-			
-			fBaseCache.push({
-				dx: base * force_x,
-				dy: base * force_y,
-			});
-		}
+		this.incMapCache = [];
 
-		this.mapCache = [];
-		for (let i = 0; i < data_length; i++) {
+		for (let i = 0; i < dataLength; i++) {
 
 			const y = Math.floor(i / width);
 			const x = i - y * width;
@@ -288,116 +321,49 @@ export default class Morphimg {
 			let incy = 0;
 
 			for (let k = 0; k < numForces; k++) {
-				const force = forces[k];
-				const fCache = fBaseCache[k];
-				const dist = Math.sqrt(Math.pow(x - force.orig_x, 2) + Math.pow(y - force.orig_y, 2));
-				//const dist = Math.sqrt(Math.pow(x - force.orig_x - force.dx, 2) + Math.pow(y - force.orig_y - force.dy, 2) );
-				const div = dist * dist / 5 + focus_val;
-				incx += fCache.dx / div;
-				incy += fCache.dy / div;
+				
+				const fIncs = this.calcForceIncs(x, y, forces[k]);
+				incx += fIncs.x;
+				incy += fIncs.y;
+
 			}
 
-			//newx = Math.max(Math.min(Math.floor(newx), width - 1), 0);
-			//newy = Math.max(Math.min(Math.floor(newy), height - 1), 0);
-
-			this.mapCache.push({ x: incx, y: incy });
+			this.incMapCache.push({ x: incx, y: incy });
 
 		}
 	}
 
 	data: Uint8ClampedArray;
 
-	deform() {
+	render() {
 
-		if (this.working) {
-			console.log('xhe!'); 
-			return;
-		}
-
-		this.working = true;
-		//this.drawImg();
-
-		//const map = [];
 		const ctx = this.ctx;
 		const width = this.width;
 		const height = this.height;
 
-		//const imageData = ctx.getImageData(0, 0, width, height);
-		//const data = imageData.data;
 		const data = this.data;
 		const newImageData = ctx.createImageData(width, height);
 		const newdata = newImageData.data;
 
-		//for (i = 0; i < newdata.length; i ++) { newdata[i] = data[i];}
+		const dataLength = data.length / 4;
 		
-		/*
-		for (const force of this.forces) {
-			const fx = force.dest_x - force.orig_x;
-			const fy = force.dest_y - force.orig_y;
-			//dist = 0//const dist = Math.sqrt(Math.pow(x - force.orig_x, 2) + Math.pow(y - force.orig_y, 2) );
-			const distort = uvmode * percentage * force_mult / focus_val;
-			force.dx = distort * fx;
-			force.dy = distort * fy;
-		}
-		*/
-
-		const data_length = data.length / 4;
-		//let prev_newx = 0, prev_newy = [];
-		//for (i=0; i<width; i++) prev_newy[i] = 0;
-		const uvmode = this.uvmode;
 		const percentage = this.percentage;
-		const force_mult = this.force_mult;
-		const focus_val = this.focus_val;
-		const forces = this.forces;
 
-		const fBaseCache = [];
-		const numForces = forces.length;
-
-		
-
-		for (let i = 0; i < data_length; i++) {
+		for (let i = 0; i < dataLength; i++) {
 
 			const y = Math.floor(i / width);
 			const x = i - y * width;
 
-			const mapping = this.mapCache[i];
+			const mapping = this.incMapCache[i];
 			let newx = x + percentage * mapping.x;
 			let newy = y + percentage * mapping.y;
-			/*
-
-			for (let k = 0; k < numForces; k++) {
-				const force = forces[k];
-				const fCache = fBaseCache[k];
-				const dist = Math.sqrt(Math.pow(x - force.orig_x, 2) + Math.pow(y - force.orig_y, 2));
-				//const dist = Math.sqrt(Math.pow(x - force.orig_x - force.dx, 2) + Math.pow(y - force.orig_y - force.dy, 2) );
-				const div = dist * dist / 5 + focus_val;
-				newx += fCache.dx / div;
-				newy += fCache.dy / div;
-			}
-			//newx = Math.floor(newx);
-			//newy = Math.floor(newy);
-
-			*/
+		
 			newx = Math.max(Math.min(Math.floor(newx), width - 1), 0);
 			newy = Math.max(Math.min(Math.floor(newy), height - 1), 0);
 
-			//if (newx - prev_newx > 1)
-
-			/*
-			for (let fix_x = prev_newx; fix_x <= newx; fix_x++) {
-				//for (let fix_y = prev_newy[fix_x]; fix_y <= newy; fix_y++) {
-					const offset_n = (y * width + x) * 4;
-					for (k = 0; k < 4; k++) {
-						newdata[4 * (newy * width + fix_x) + k] = data[offset_n + k];
-					}
-				//}
-				//prev_newy[fix_x] = fix_y;
-			}
-			prev_newx = newx;
-			*/
+			
 			const offset_pos = (y * width + x) * 4 ;
 			const offset_pos_n = (newy * width + newx) * 4;
-			//const offset_pos_n = (this.mapCache[i]) * 4;
 			for (let k = 0; k < 4; k++)
 				if (this.uvmode === 1)
 					newdata[offset_pos_n + k] = data[offset_pos + k];
@@ -406,27 +372,9 @@ export default class Morphimg {
 			//map[2 * (y * width + x)] = newx;
 			//map[2 * (y * width + x) + 1] = newy;
 		}
-		/*
-		for (let j = 0; j < height; j ++) {
-			const offset_j = j * width;
-			for (i = 0; i < width; i ++) {
-				const offset_i = 2*(offset_j + i);
-				//if (map[offset_i] === undefined) map[offset_i];
-				const u = map[offset_i];
-				const v = map[offset_i + 1];
-				const nx = Math.floor(u);
-				const ny = Math.floor(v);
-				const offset_n = (ny*width + nx) * 4;
-				for (k = 0; k < 4; k++) {
-					const vectorized_pos = offset_i * 2 + k;
-					newdata[vectorized_pos] = data[offset_n + k];
-					//newdata[vectorized_pos] = data[offset_n + k];// + data[(ny - 1) * 4 * width + nx * 4 + k] +
-											  //data[(ny + 1) * 4 * width + nx * 4 + k] + data[ny * 4 * width + (nx-1) * 4 + k]) / 4;
-				}
-			}
-		}*/
+		
 		ctx.putImageData(newImageData, 0, 0);
-		this.working = false;
+
 	}
 
 	animate() {
@@ -438,7 +386,7 @@ export default class Morphimg {
 		const animation = () => {
 			if (!this.animating) return;
 			this.percentage += 0.001 * this.animation_speed;//;1 - Math.pow(Math.sin(gr += 0.015), 6);
-			this.deform();
+			this.render();
 			if (this.percentage >= 1) {
 				this.percentage = 1;
 				this.animating = false;
@@ -453,9 +401,11 @@ export default class Morphimg {
 
 
 	deleteAllForces() {
+		
 		this.forces = [];
 		this.selected_force_id = -1;
-		this.deform();
+		this.refresh();
+
 	}
 
 
